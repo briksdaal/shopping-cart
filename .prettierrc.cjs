a8f27f4d65ee0be04616152bccce19956b5f52d7
briksdaal <102250834+briksdaal@users.noreply.github.com>
2023-08-27 16:34:41 +0300

    Initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'none',
+  jsxBracketSameLine: true,
+  plugins: ['prettier-plugin-tailwindcss']
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+# React + Vite Setup
+
+For forking to new projects. Includes:
+
+- react
+- vite
+- eslint
+- prettier
+- vitest
+- react testing library
+- tailwind css
