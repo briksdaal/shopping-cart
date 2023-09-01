79341cc5436fea43555dce0be2209e3952231346
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-09-01 16:13:16 +0300

    Change game prop to product

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -3,20 +3,20 @@ import prepareText from '../helpers/prepareText';
 import PropTypes from 'prop-types';
 import AddToCartButton from './AddToCartButton';
 
-function ProductPage({ game }) {
+function ProductPage({ product }) {
   return (
     <div>
-      <img src={game.background_image} alt={game.name} />
-      <h1>{game.name}</h1>
-      <p>{prepareText(game.description_raw)}</p>
-      <p>${getPrice(game.id)}</p>
+      <img src={product.background_image} alt={product.name} />
+      <h1>{product.name}</h1>
+      <p>{prepareText(product.description_raw)}</p>
+      <p>${getPrice(product.id)}</p>
       <AddToCartButton />
     </div>
   );
 }
 
 ProductPage.propTypes = {
-  game: PropTypes.object
+  product: PropTypes.object
 };
 
 export default ProductPage;
