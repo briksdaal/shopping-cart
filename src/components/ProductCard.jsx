8a14414de98bdc2ead39e9abf74c906d5a6906fb
briksdaal <102250834+briksdaal@users.noreply.github.com>
2023-11-21 18:02:39 +0200

    Add 'X' button for removing item from cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,22 +2,52 @@ import PropTypes from 'prop-types';
 import getPrice from '../helpers/generatePriceFromId';
 import { Link } from 'react-router-dom';
 import AddToCartButton from './AddToCartButton';
+import { IoMdClose } from 'react-icons/io';
 
-function ProductCard({ product, qty, onChange }) {
+function ProductCard({ product, qty, onChange, cartView }) {
   return (
-    <div className="shadow-3xl flex flex-col gap-4 rounded-md bg-white p-5">
+    <div
+      className={
+        !cartView
+          ? 'flex flex-col gap-4 rounded-md bg-white p-5 shadow-3xl'
+          : 'relative grid grid-cols-[auto_1fr] gap-4 rounded-md bg-white p-3 shadow-3xl'
+      }>
+      {cartView && qty && (
+        <button
+          className="absolute right-4 top-4 text-neutral-400 hover:text-neutral-700"
+          data-testid="remove-from-cart"
+          onClick={() => onChange(0)}>
+          <IoMdClose />
+        </button>
+      )}
       <Link to={`/shop/${product.id}`}>
         <img
           src={product.background_image}
           alt={product.name}
-          className="h-64 w-full object-cover"
+          className={
+            !cartView ? 'h-64 w-full object-cover' : 'h-16 w-32 object-cover'
+          }
         />
       </Link>
       <Link to={`/shop/${product.id}`}>
-        <h3 className="h-16 text-2xl font-bold">{product.name}</h3>
+        <h3
+          className={
+            !cartView ? 'h-16 text-2xl font-bold' : 'mr-6 text-xl font-bold'
+          }>
+          {product.name}
+        </h3>
       </Link>
-      <p className="text-lg text-red-600">${getPrice(product.id)}</p>
-      <AddToCartButton qty={qty} onChange={onChange} />
+      <p
+        className={
+          !cartView
+            ? 'text-lg text-red-600'
+            : 'self-center text-lg text-red-600'
+        }>
+        ${getPrice(product.id)}
+      </p>
+      <div>
+        <AddToCartButton qty={qty} onChange={onChange} cartView={cartView} />
+      </div>
     </div>
   );
 }
@@ -25,6 +55,7 @@ function ProductCard({ product, qty, onChange }) {
 ProductCard.propTypes = {
   product: PropTypes.object,
   qty: PropTypes.number,
-  onChange: PropTypes.func
+  onChange: PropTypes.func,
+  cartView: PropTypes.bool
 };
 export default ProductCard;
--- src/tests/ProductCard.test.jsx
+++ src/tests/ProductCard.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { expect } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import ProductCard from '../components/ProductCard';
 import getPrice from '../helpers/generatePriceFromId';
 
@@ -10,44 +11,103 @@ const game = {
   background_image: 'https://imagesbank.com/2349302'
 };
 
-const renderCard = () => {
-  render(<ProductCard product={game} />, {
-    wrapper: ({ children }) => {
-      return <BrowserRouter>{children}</BrowserRouter>;
+const onChange = vi.fn();
+
+const renderCard = (qty, cartView) => {
+  render(
+    <ProductCard
+      product={game}
+      qty={qty}
+      onChange={onChange}
+      cartView={cartView}
+    />,
+    {
+      wrapper: ({ children }) => {
+        return <BrowserRouter>{children}</BrowserRouter>;
+      }
     }
-  });
+  );
 };
 
-it('Renders card title', () => {
-  renderCard();
-
-  expect(screen.getByRole('heading', { name: game.name })).toBeInTheDocument();
+afterEach(() => {
+  vi.resetAllMocks();
 });
 
-it('Renders price correctly', () => {
-  const price = getPrice(game.id).toString();
-  renderCard();
+describe('Render tests', () => {
+  it('Renders card title', () => {
+    renderCard();
 
-  expect(screen.getByText(price, { exact: false })).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: game.name })
+    ).toBeInTheDocument();
+  });
+
+  it('Renders price correctly', () => {
+    const price = getPrice(game.id).toString();
+    renderCard();
+
+    expect(screen.getByText(price, { exact: false })).toBeInTheDocument();
+  });
+
+  it('Renders image', () => {
+    renderCard();
+
+    expect(screen.getByRole('img').src).toEqual(game.background_image);
+    expect(screen.getByRole('img').alt).toEqual(game.name);
+  });
+
+  it('Renders add to cart button', () => {
+    renderCard();
+
+    expect(screen.getByRole('button', 'Add to Cart')).toBeInTheDocument();
+  });
+
+  it('Renders a link to the correct page', () => {
+    renderCard();
+    const links = screen.getAllByRole('link');
+    links.forEach((link) => {
+      expect(link).toHaveAttribute('href', `/shop/${game.id}`);
+    });
+  });
 });
 
-it('Renders image', () => {
-  renderCard();
+describe('X button tests', () => {
+  it("X button doesn't show on regular view with no quantity", async () => {
+    renderCard();
+    const xButton = screen.queryByTestId('remove-from-cart');
 
-  expect(screen.getByRole('img').src).toEqual(game.background_image);
-  expect(screen.getByRole('img').alt).toEqual(game.name);
-});
+    expect(xButton).not.toBeInTheDocument();
+  });
 
-it('Renders add to cart button', () => {
-  renderCard();
+  it("X button doesn't show on regular view even with quantity", async () => {
+    renderCard(3);
+    const xButton = screen.queryByTestId('remove-from-cart');
 
-  expect(screen.getByRole('button', 'Add to Cart')).toBeInTheDocument();
-});
+    expect(xButton).not.toBeInTheDocument();
+  });
 
-it('Renders a link to the correct page', () => {
-  renderCard();
-  const links = screen.getAllByRole('link');
-  links.forEach((link) => {
-    expect(link).toHaveAttribute('href', `/shop/${game.id}`);
+  it("X button doesn't show on cart view with no quantity", async () => {
+    renderCard(0, true);
+    const xButton = screen.queryByTestId('remove-from-cart');
+
+    expect(xButton).not.toBeInTheDocument();
+  });
+
+  it('X button shows on cart view with quantity', async () => {
+    renderCard(3, true);
+    const xButton = screen.getByTestId('remove-from-cart');
+
+    expect(xButton).toBeInTheDocument();
+  });
+
+  it('Clicking X button calls onChange with 0', async () => {
+    const user = userEvent.setup();
+    renderCard(3, true);
+    const xButton = screen.getByTestId('remove-from-cart');
+
+    await user.click(xButton);
+
+    expect(onChange).toHaveBeenCalledOnce();
+    expect(onChange).toHaveBeenCalledWith(0);
   });
 });
